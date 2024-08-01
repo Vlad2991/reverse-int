54a8d14ad4550d797a68d696bd5c56a7196e0f00
Vlad2991 <vlad29911710@gmail.com>
2024-08-01 09:14:56 +0300

    revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
+// module.exports = function reverse (n) {
   
-}
+// }
+function reverse(num) {
+    // Преобразуем число в строку
+    let numStr = num.toString();
+    
+    // Разбиваем строку на массив символов, переворачиваем его и объединяем обратно в строку
+    let reversedStr = numStr.split('').reverse().join('');
+    
+    // Преобразуем перевернутую строку обратно в число и возвращаем
+    return parseInt(reversedStr, 10);
+  }
+  
+  module.exports = reverse;
